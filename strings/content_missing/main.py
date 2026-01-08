17c6d2aecfa4b9b32829e97a4ddeddec17d70a16
Ndijdo <>
2026-01-08 18:59:46 +0000

    Content missing

---
+++ strings/content_missing/main.py
@@ -0,0 +1,11 @@
+string = "It can be painful to learn from mistakes"
+
+# Extract necessary words
+learn = string[string.index('learn'):string.index('learn') + ___]
+painful = string[string.index('painful'):string.index('painful') + ___]
+mistakes = string[string.index('mistakes'):string.index('mistakes') + ]
+
+
+print("The variable learn equals:", learn)
+print("The variable painful equals:", painful)
+print("The variable mistakes equals:", mistakes)
