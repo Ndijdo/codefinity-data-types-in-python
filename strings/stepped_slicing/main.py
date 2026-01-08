9d25e485bcace4572499335f971c72ebaf10836a
Ndijdo <>
2026-01-08 18:29:56 +0000

    Stepped slicing

---
+++ strings/stepped_slicing/main.py
@@ -0,0 +1,6 @@
+filename = "Annual Budget Report 2024"
+filename_lengte = len(filename)
+
+year_code = filename[-2:-5:-2]
+#print(filename_lengte)
+print("Variable sliced_string equals:", year_code)
